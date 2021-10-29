ed6a8230f845575806dd93ed3369dc25db9bfd60
MonkeyAndEgg <412793874@qq.com>
2021-10-28 22:55:43 -0700

    added routing for cart and updated styling for cart buttons

--- src/app/components/cart/cart.module.ts
+++ src/app/components/cart/cart.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from "@angular/core";
 import { CartComponent } from "./cart.component";
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
 import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from "src/app/app-routing.module";
 
 @NgModule({
   declarations: [
     CartComponent
   ],
   imports: [
+    AppRoutingModule,
     CommonModule,
-    MatButtonModule,
-    MatListModule
+    MatButtonModule
   ],
   exports: [
     CartComponent
