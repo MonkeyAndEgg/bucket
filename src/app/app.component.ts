5ec1ec21ba742ce6c44fa6997efd03f8bfd33f4f
MonkeyAndEgg <412793874@qq.com>
2021-11-13 20:59:25 -0800

    added scrollbar when resize browser window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'bucket';
+  private currentWindowWidth: number | undefined;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.determineWhetherEnableScrollbar();
+  }
+
+  ngOnInit(): void {
+    this.determineWhetherEnableScrollbar();
+  }
+
+  // display a horizontal scrollbar when users resize the browser window
+  private determineWhetherEnableScrollbar(): void {
+    this.currentWindowWidth = window.innerWidth;
+    if (this.currentWindowWidth && this.currentWindowWidth < screen.width) {
+      document.body.style.overflow = 'auto';
+      document.body.style.minWidth = screen.width + 'px';
+    } else {
+      document.body.style.overflow = '';
+      document.body.style.minWidth = '';
+    }
+  }
 }
