74ed1bdb023e3dd319dfd9b5c7928a60e7b2bea2
MonkeyAndEgg <412793874@qq.com>
2021-11-27 23:34:22 -0800

    removed angular material form field

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -4,9 +4,6 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from "src/app/app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { AuthModule } from "../auth/auth.module";
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
 import { ReactiveFormsModule } from "@angular/forms";
 import { MatTooltipModule } from '@angular/material/tooltip';
 
@@ -18,9 +15,6 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     AppRoutingModule,
     AuthModule,
     BrowserModule,
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
     MatToolbarModule,
     MatTooltipModule,
     ReactiveFormsModule
