5a67cc804395804cfdd1743c642c6df95b253a88
MonkeyAndEgg <412793874@qq.com>
2022-02-05 16:58:51 -0800

    added order-detail test

--- src/app/components/view-order-detail/view-order-detail.component.spec.ts
+++ src/app/components/view-order-detail/view-order-detail.component.spec.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
@@ -11,12 +13,33 @@ import { ViewOrderDetailService } from './view-order-detail.service';
 describe('ViewOrderDetailComponent', () => {
   let component: ViewOrderDetailComponent;
   let fixture: ComponentFixture<ViewOrderDetailComponent>;
+  let el: DebugElement;
+  let service: any;
+  const mockedData = {
+    address: {
+      shipping: {
+        address: "6363 Skaha Crescent",
+        city: "Richmond",
+        postCode: "V7C2R2",
+        state: "BC"
+      }
+    },
+    createdAt: "2022-02-06T00:28:35.736Z",
+    products: [{
+      product: {
+        name: "Play Station 5"
+      },
+      quantity: 3
+    }],
+    status: "Wait To Deliver",
+    total: 1799.97,
+    userId: "61b980d2e1a9090cfdccc493"
+  }
 
   beforeEach(waitForAsync(() => {
     const serviceSpy = jasmine.createSpyObj('ViewOrderDetailService', {
       'loadOrderById': null,
-      'getSelectedProduct': of(undefined),
-      'getSelectedOrder': of(undefined)
+      'getSelectedOrder': of(mockedData)
     });
     TestBed.configureTestingModule({
       declarations: [ ViewOrderDetailComponent ],
@@ -34,11 +57,32 @@ describe('ViewOrderDetailComponent', () => {
     .compileComponents().then(() => {
       fixture = TestBed.createComponent(ViewOrderDetailComponent);
       component = fixture.componentInstance;
+      el = fixture.debugElement;
+      service = TestBed.inject(ViewOrderDetailService);
       fixture.detectChanges();
     });
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display proper labels with values', () => {
+    const labels = el.queryAll(By.css('.column-field label'));
+    expect(labels.length).toBe(4);
+    expect(labels[0].nativeElement.textContent).toContain('Shipping Address');
+    expect(labels[1].nativeElement.textContent).toContain('Order Date');
+    expect(labels[2].nativeElement.textContent).toContain('Order Items');
+    expect(labels[3].nativeElement.textContent).toContain('Order Summary');
+
+    const fieldValues = el.queryAll(By.css('.column-field span'));
+    expect(fieldValues.length).toBe(7);
+    expect(fieldValues[0].nativeElement.textContent).toContain('6363 Skaha Crescent');
+    expect(fieldValues[1].nativeElement.textContent).toContain('Richmond');
+    expect(fieldValues[2].nativeElement.textContent).toContain('BC');
+    expect(fieldValues[3].nativeElement.textContent).toContain('V7C2R2');
+    expect(fieldValues[4].nativeElement.textContent).toContain('Saturday, February 5, 2022');
+    expect(fieldValues[5].nativeElement.textContent).toContain('Play Station 5 * (3)');
+    expect(fieldValues[6].nativeElement.textContent).toContain('$1799.97');
   });
 });
--- src/app/components/view-order-detail/view-order-detail.service.ts
+++ src/app/components/view-order-detail/view-order-detail.service.ts
@@ -3,17 +3,13 @@ import { Store } from "@ngrx/store";
 import { Observable } from "rxjs";
 import { Order } from "src/app/models/order";
 import { loadOrderById } from "src/app/store/order/order.actions";
-import { selectOrderInView, selectOrders } from "src/app/store/order/order.selector";
+import { selectOrderInView } from "src/app/store/order/order.selector";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewOrderDetailService {
   constructor(private store: Store) {}
-
-  getUserOrders(): Observable<Order[]> {
-    return this.store.select(selectOrders);
-  }
 
   getSelectedOrder(): Observable<Order | undefined> {
     return this.store.select(selectOrderInView);
